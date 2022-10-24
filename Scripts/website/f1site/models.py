9a0b3171c856b0273beec1fd8363e26b1a216306
Reyke92 <50183223+Reyke92@users.noreply.github.com>
2022-10-24 18:59:33 -0500

    Updated models (after meeting)

--- Scripts/website/f1site/models.py
+++ Scripts/website/f1site/models.py
@@ -1,8 +1,11 @@
+from asyncio.windows_events import NULL
 from django.db import models
 
 
 class GP(models.Model):
     # id = models.BigAutoField() <--- PRIMARY KEY
+    name = models.CharField(max_length=60, null=False)
+    event = models.ForeignKey('Event', null=False, on_delete=models.CASCADE)
     track = models.ForeignKey('Track', null=False, on_delete=models.DO_NOTHING)
     date = models.DateTimeField(null=False)
 
@@ -12,76 +15,44 @@ class GP(models.Model):
 
 class Track(models.Model):
     # id = models.BigAutoField() <--- PRIMARY KEY
-    name = models.CharField(max_length=60, null=False)
+    name = models.CharField(max_length=40, null=False)
     location = models.ForeignKey('Location', null=False, on_delete=models.DO_NOTHING)
+    country = models.CharField(max_length=25, null=False)
 
     def __str__(self):
-        return self.name
+        return f'{self.name} ({self.country})'
 
 
-class Location(models.Model):
+class Event(models.Model):
     # id = models.BigAutoField() <--- PRIMARY KEY
-    address = models.CharField(max_length=100, null=False)
-    zipcode = models.PositiveSmallIntegerField(null=False)
-    state = models.ForeignKey('State', null=False, on_delete=models.DO_NOTHING)
-
-    def __str__(self):
-        return f'{self.address}, {self.state.acronym} {self.zipcode}'
-
-
-class State(models.Model):
-    # id = models.BigAutoField() <--- PRIMARY KEY
-    acronym = models.CharField(max_length=2, null=False)
-    name = models.CharField(max_length=14, null=False)
-
-    def __str__(self):
-        return self.name
-
-
-# Qualification Race.
-class QualRace(models.Model):
-    # id = models.BigAutoField() <--- PRIMARY KEY
-    gp = models.ForeignKey('GP', null=False, on_delete=models.CASCADE)
     driver = models.ForeignKey('Driver', null=False, on_delete=models.CASCADE)
-    # Ranking: how well the driver did in this race.
-    ranking = models.PositiveSmallIntegerField(null=False)
-    # Points Earned: the amount of points earned from this particular race.
-    pointsEarned = models.PositiveSmallIntegerField(null=False)
+    
     # Driver Team: the team the driver was playing for within this particular race.
     driverTeam = models.ForeignKey('Team', null=False, on_delete=models.CASCADE)
+    
+    # Qualificaiton Race Rank: how well the driver did in the qualification race.
+    qualRank = models.PositiveSmallIntegerField(null=False)
+
+    # Sprint Race Rank: how well the driver did in the sprint race.
+    sprintRank = models.PositiveSmallIntegerField(null=True)
+
+    # (Final) Race Rank: how well the driver did in the final race.
+    raceRank = models.PositiveSmallIntegerField(null=False)
+
+    # Qualification Points: the amount of points earned from the qualification race.
+    qualPoints = models.PositiveSmallIntegerField(null=False)
+
+    # Sprint Points: the amount of points earned from the sprint race.
+    sprintPoints = models.PositiveSmallIntegerField(null=True)
+
+    # (Final) Race Points: the amount of points earned from the the final race.
+    racePoints = models.PositiveSmallIntegerField(null=False)
 
     def __str__(self):
-        return f'{self.gp} ({self.driver}) ({self.driverTeam})'
+        totalPoints = self.racePoints + self.qualPoints
+        if (self.sprintPoints != NULL): totalPoints += self.sprintPoints
 
-
-class SprintRace(models.Model):
-    # id = models.BigAutoField() <--- PRIMARY KEY
-    gp = models.ForeignKey('GP', null=False, on_delete=models.CASCADE)
-    driver = models.ForeignKey('Driver', null=False, on_delete=models.CASCADE)
-    # Ranking: how well the driver did in this race.
-    ranking = models.PositiveSmallIntegerField(null=False)
-    # Points Earned: the amount of points earned from this particular race.
-    pointsEarned = models.PositiveSmallIntegerField(null=False)
-    # Driver Team: the team the driver was playing for within this particular race.
-    driverTeam = models.ForeignKey('Team', null=False, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.gp} ({self.driver}) ({self.driverTeam})'
-
-
-class Race(models.Model):
-    # id = models.BigAutoField() <--- PRIMARY KEY
-    gp = models.ForeignKey('GP', null=False, on_delete=models.CASCADE)
-    driver = models.ForeignKey('Driver', null=False, on_delete=models.CASCADE)
-    # Ranking: how well the driver did in this race.
-    ranking = models.PositiveSmallIntegerField(null=False)
-    # Points Earned: the amount of points earned from this particular race.
-    pointsEarned = models.PositiveSmallIntegerField(null=False)
-    # Driver Team: the team the driver was playing for within this particular race.
-    driverTeam = models.ForeignKey('Team', null=False, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.gp} ({self.driver}) ({self.driverTeam})'
+        return f'({self.driver}) ({self.driverTeam}) ({totalPoints} points total)'
 
 
 class Driver(models.Model):
@@ -97,6 +68,7 @@ class Driver(models.Model):
 class Team(models.Model):
     # id = models.BigAutoField() <--- PRIMARY KEY
     name = models.CharField(max_length=30, null=False)
+    hq = models.CharField(max_length=25, null=False)
 
     def __str__(self):
         return f'{self.name}'
